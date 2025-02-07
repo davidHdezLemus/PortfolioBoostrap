c076e9437f459a053e958e3b011757a19e34cdef
david hdez lemus <drhernandezl01@iesalbarregas.es>
2025-02-07 18:09:41 +0100

    hotfix

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 const navbarToggle = document.getElementById("navbar-toggle");
 const navbarMenu = document.getElementById("navbar-menu");
 
-// Alternar menú en móviles
 navbarToggle.addEventListener("click", () => {
   navbarMenu.classList.toggle("hidden");
 });
@@ -9,23 +8,21 @@ navbarToggle.addEventListener("click", () => {
 document.addEventListener('DOMContentLoaded', function () {
   const skillsContainer = document.querySelector('#skills .skills-container');
 
-  // Función para comprobar si la sección de habilidades es visible
   function checkVisibility() {
     if (!skillsContainer) return;
 
     const rect = skillsContainer.getBoundingClientRect();
-    const isVisible = rect.top < window.innerHeight * 0.5; // Activa cuando la mitad de la sección es visible
+    const isVisible = rect.top < window.innerHeight * 0.5;
 
     if (isVisible) {
       skillsContainer.classList.add('animate-skills');
-      window.removeEventListener('scroll', checkVisibility); // Deja de escuchar el scroll después de activarlo
+      window.removeEventListener('scroll', checkVisibility);
     }
   }
 
   window.addEventListener('scroll', checkVisibility);
-  checkVisibility(); // Verifica al cargar la página
+  checkVisibility();
 
-  // Efecto de máquina de escribir
   const textElement = document.getElementById("typewriter");
   const text = "David Hernández Lemus";
   let index = 0;
